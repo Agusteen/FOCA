13b8e60f08148550ad488cdd49d84232e16cbd3a
Agusteen <agustin.php@gmail.com>
2015-10-01 21:56:07 -0300

    Agregado el "datepicker" y modificado el checkbox de ABMC_Clientes

--- FOCA gadgets V1/ABMC_Clientes.aspx.designer.cs
+++ FOCA gadgets V1/ABMC_Clientes.aspx.designer.cs
@@ -74,14 +74,5 @@ namespace FOCA_gadgets_V1 {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtFechaNacimiento;
-        
-        /// <summary>
-        /// Control chboxPreferencial.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox chboxPreferencial;
     }
 }
